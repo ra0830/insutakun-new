e5bd83113bcd295371419aaf9a024af985aa0ceb
asanorintaro <asanorintaro19880830@gmail.com>
2018-08-29 13:22:07 +0900

    change config2

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Instakun
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
