929069844816a47b31386dbe13084e5834568aa2
asanorintaro <asanorintaro19880830@gmail.com>
2018-08-31 15:23:11 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :favorites, only:[:create, :destroy, :index]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  #mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
 end
